796382efaf02616bfe4973e0e1bfacb571368d52
Andre <andre.eng1313@gmail.com>
2023-05-15 15:13:01 -0300

    fixing warning messages on hotel page

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,38 +1,106 @@
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import useToken from '../../../hooks/useToken';
 import HotelsContainer from './HotelsContainer';
 import RoomsContainer from './RoomsContainer';
-import { useState } from 'react';
-import useRoomsBooked from '../../../hooks/api/useRoomsBooked';
+import useRoomsBooked from '../../../hooks/api/useRoomsBooked.js';
+import styled from 'styled-components';
 
 export default function Hotel() {
+  const [paymentConfirmed, setPaymentConfirmed] = useState(false);
   const [rooms, setRooms] = useState([]);
   const { roomsBooked } = useRoomsBooked();
+  const [ticketIncludesHotel, setTicketIncludesHotel] = useState(false);
 
   const allBookedRooms = roomsBooked?.map((r) => r.roomId);
+  const token = useToken();
+
+  useEffect(() => {
+    async function checkPaymentStatus() {
+      try {
+        // Verificando se o token está presente
+        if (!token) {
+          setPaymentConfirmed(false);
+          setTicketIncludesHotel(false);
+          return;
+        }
+
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        };
+
+        // Fazer a solicitação GET para verificar o status do pagamento
+        const response = await axios.get('http://localhost:4000/payments?ticketId=1', config);
+        const data = response.data;
+        console.log(data);
+
+        // Verificar se o pagamento foi confirmado
+        if (data.paymentConfirmed) {
+          setPaymentConfirmed(true);
+        } else {
+          setPaymentConfirmed(false);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+
+    async function checkTicketIncludesHotel() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+      try {
+        const response = await axios.get('http://localhost:4000/tickets/types', config);
+        const ticketTypes = response.data;
+        console.log(ticketTypes);
+        const includesHotel = ticketTypes.some((type) => type.includesHotel);
+        setTicketIncludesHotel(includesHotel);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    checkTicketIncludesHotel();
+    checkPaymentStatus();
+  }, [token]);
 
   return (
-    <Container>
-      <h1>Escolha de hotel e quarto</h1>
-      <h3>Primeiro, escolha seu hotel</h3>
-      <HotelsContainer rooms={rooms} setRooms={setRooms} />
-
-      {rooms?.length > 0 && (
-        <>
-          <h3>Ótima pedida! Agora escolha seu quarto:</h3>
-          <RoomsContainer rooms={rooms} allBookedRooms={allBookedRooms} />
-        </>
+    <div>
+      {!paymentConfirmed && (
+        <ContainerWarning>
+          <p>Você precisa ter confirmado pagamento antes de fazer a escolha da hospedagem.</p>
+        </ContainerWarning>
       )}
-    </Container>
+  
+      {paymentConfirmed && !ticketIncludesHotel && (
+        <ContainerWarning>
+          <p>Sua modalidade de ingresso não inclui hospedagem.</p>
+          <div>
+            <p>Prossiga para a escolha de atividades.</p>
+          </div>
+        </ContainerWarning>
+      )}
+  
+      {paymentConfirmed && ticketIncludesHotel && (
+        <Container>
+          <h1>Escolha de hotel e quarto</h1>
+          <h3>Primeiro, escolha seu hotel</h3>
+          <HotelsContainer rooms={rooms} setRooms={setRooms} />
+  
+          {rooms?.length > 0 && (
+            <>
+              <h3>Ótima pedida! Agora escolha seu quarto:</h3>
+              <RoomsContainer rooms={rooms} allBookedRooms={allBookedRooms} />
+            </>
+          )}
+        </Container>
+      )}
+    </div>
   );
 }
-
-/* <ContainerWarning>
-    <p>Sua modalidade de ingresso não inclui hospedagem Prossiga para a escolha de atividades</p>
-  </ContainerWarning> */
-
-//   <ContainerWarning>
-//   <p>Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem</p>
-// </ContainerWarning>
 
 const Container = styled.div`
   padding: 20px 34px;
@@ -58,6 +126,7 @@ const ContainerWarning = styled.div`
   justify-content: center;
   align-items: center;
   height: 100%;
+  margin-top: 200px;
   p {
     margin: auto;
     font-weight: 400;
