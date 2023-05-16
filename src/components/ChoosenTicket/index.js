3005fca9851d12ef4e749d1690bb3a6d1b123ba1
jmarcossss <joaomalcantaraaa@gmail.com>
2023-05-16 02:52:43 -0300

    chore(*): commiting before git pull drivent sprint 1

--- src/components/ChoosenTicket/index.js
+++ src/components/ChoosenTicket/index.js
@@ -1,62 +1,62 @@
-import React from 'react';
-import styled from 'styled-components';
+// import React from 'react';
+// import styled from 'styled-components';
 
-function ChoosenTicket() {
-  return (
-    <ChoosenTicketDiv>
-      <h5>
-        Ingresso escolhido
-      </h5>
-      <Container>
-        <h6>Presencial + Com Hotel</h6>
-        {/* <h6>Presencial + Sem Hotel</h6>
-        <h6>Online</h6> */}
-        <p>R$ 600</p>
-      </Container>
-    </ChoosenTicketDiv>
-  );
-}
+// function ChoosenTicket() {
+//   return (
+//     <ChoosenTicketDiv>
+//       <h5>
+//         Ingresso escolhido
+//       </h5>
+//       <Container>
+//         <h6>Presencial + Com Hotel</h6>
+//         {/* <h6>Presencial + Sem Hotel</h6>
+//         <h6>Online</h6> */}
+//         <p>R$ 600</p>
+//       </Container>
+//     </ChoosenTicketDiv>
+//   );
+// }
 
-const ChoosenTicketDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  h5{
-    font-family: 'Roboto', 'sans-serif';
-    font-size: 20px;
-    font-weight: 400;
-    line-height: 23px;
-    text-align: left;
-    color: #8E8E8E;
-    margin: 27px 0px 10px 5px;
-  }
-`;
+// const ChoosenTicketDiv = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   h5{
+//     font-family: 'Roboto', 'sans-serif';
+//     font-size: 20px;
+//     font-weight: 400;
+//     line-height: 23px;
+//     text-align: left;
+//     color: #8E8E8E;
+//     margin: 27px 0px 10px 5px;
+//   }
+// `;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 290px;
-  height: 108px;
-  left: 330px;
-  top: 292px;
-  gap: 7px;
-  background: #ffeed2;
-  border-radius: 20px;
-  margin-bottom: 20px;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   width: 290px;
+//   height: 108px;
+//   left: 330px;
+//   top: 292px;
+//   gap: 7px;
+//   background: #ffeed2;
+//   border-radius: 20px;
+//   margin-bottom: 20px;
 
-  h6 {
-    font-weight: 400;
-    font-size: 16px;
-    text-align: center;
-    color: #454545;
-  }
-  p {
-    font-weight: 400;
-    font-size: 14px;
-    text-align: center;
-    color: #898989;
-  }
-`;
+//   h6 {
+//     font-weight: 400;
+//     font-size: 16px;
+//     text-align: center;
+//     color: #454545;
+//   }
+//   p {
+//     font-weight: 400;
+//     font-size: 14px;
+//     text-align: center;
+//     color: #898989;
+//   }
+// `;
 
-export default ChoosenTicket;
+// export default ChoosenTicket;
